4e47d97bcd5dbcabd4660f05dbd5fb8bbdaedbeb
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-15 08:50:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,6 +5,7 @@
 
 def meal_choice veg1, veg2, protein = 'tofu'
   puts "What a nutritious meal!"
-  puts "A plate of #{proteins} with #{veg1} and #{veg2}."
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  meal = "A plate of #{proteins} with #{veg1} and #{veg2}."
+  puts meal
+  meal
 end
