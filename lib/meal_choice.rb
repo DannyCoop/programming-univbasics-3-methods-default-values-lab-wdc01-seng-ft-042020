81993f84a0e8048d06a2e14c2a3ef36de013a26f
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-15 08:39:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,7 @@ proteins = lentils
 veg1 = corn
 veg2 = carrots
 
-def meal_choice veg1, veg2, protein = "tofu"
+def meal_choice veg1, veg2, protein = 'tofu'
   puts "What a nutritious meal!"
   puts "A plate of #{proteins} with #{veg1} and #{veg2}."
   return "A plate of #{protein} with #{veg1} and #{veg2}."
